3cee902edf7b494ece63e7554e33a0eee7e88366
Yulissa Rivas <lovnelies@gmail.com>
2025-09-10 00:31:11 -0300

    minor changes :(

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,37 +1,41 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 
 const MobileMenu = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-        <div className=""> 
-            <div
-  className="flex flex-col gap-[4.5px] cursor-pointer"
-  onClick={() => setIsOpen((prev) => !prev)}
->
-  <div className="w-6 h-1 bg-blue-500 rounded-md" />
-  <div className="w-6 h-1 bg-blue-500 rounded-md" />
-  <div className="w-6 h-1 bg-blue-500 rounded-md" />
-</div>
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="md:hidden">
+      <div
+        className="flex flex-col gap-[4.5px] cursor-pointer"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+      {/*Animación X navbar*/}
+        <div className={`w-6 h-1 bg-blue-500 rounded-sm" ${
+            isOpen ? "rotate-45" : ""
+        } origin-left ease-in-out duration-500`}
+        />
+        <div className={`w-6 h-1 bg-blue-500 rounded-sm" ${
+            isOpen ? "opacity-0" : ""
+        } ease-in-out duration-500`}
+        />
+        <div className={`w-6 h-1 bg-blue-500 rounded-sm" ${
+            isOpen ? "-rotate-45" : ""
+        } origin-left ease-in-out duration-500`}
+        />
+      </div>
 
-            {isOpen && (
-           <div
-           className="absolute left-0 top-24 w-full bg-white"
-           style={{ height: "calc(100vh - 96px)" }}
-         >
-         
-                <ul className="flex flex-col gap-4">
-                    <li className="hover:text-blue-500 cursor-pointer">Home</li>
-                    <li className="hover:text-blue-500 cursor-pointer">About</li>
-                    <li className="hover:text-blue-500 cursor-pointer">Services</li>
-                    <li className="hover:text-blue-500 cursor-pointer">Contact</li>
-                </ul>
-            </div>
-        )
-        }
+      {isOpen && (
+        <div className="absolute left-0 top-24 w-full bg-white h-[calc(100vh-96px)] flex flex-col items-center justify-center gap-8 font-medium text-xl">
+          <ul className="flex flex-col gap-4">
+            <li className="hover:text-blue-500 cursor-pointer">Home</li>
+            <li className="hover:text-blue-500 cursor-pointer">About</li>
+            <li className="hover:text-blue-500 cursor-pointer">Services</li>
+            <li className="hover:text-blue-500 cursor-pointer">Contact</li>
+          </ul>
         </div>
-        
-    );
-}
+      )}
+    </div>
+  );
+};
 export default MobileMenu;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,33 @@
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className=" h-24 flex items-center justify-between">
-      <div className="">
-        <Link href="/">Holi</Link>
+      <div className="md:hidden lg:block w-[20%]">
+        <Link href="/" className="font-bold text-xl text-blue-400">
+        Hellow
+        </Link>
       </div>
-      <div className="hidden"></div>
-      <div className=""> 
+      <div className="hidden md:flex w-[50%]">
+        {/*LINKS*/}
+        <div className="flex gap-6 text-gray-600">
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/home.png" alt="homepage" width={16} height={16} className="w-4 h-4"/>
+          <span> Homepage</span>
+        </Link>
+        <Link href="/"className="flex items-center gap-2">
+          <Image src="/friends.png" alt="homepage" width={16} height={16} />
+          <span> friends</span>
+        </Link>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/stories.png" alt="homepage" width={16} height={16}/>
+          <span> stories</span>
+        </Link>
+        </div>
+      </div>
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end"> 
         <MobileMenu/>
         </div>
     </div>
